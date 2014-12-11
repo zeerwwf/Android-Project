89feddeb837ff3b5e4e4a218d4a03afa9671c0ae
AlanChan08 <alan_chan08@hotmail.com>
2014-12-11 23:25:51 +0800

    .

--- Homework/Homework1/hello.cpp
+++
@@ -1,7 +0,0 @@
-#include <utils/Log.h>
-#include <utils/String16.h>
-
-int main(int argc, char **argv)
-{
-  ALOGE("Test Hello!"); 
-}
--- Homework/Homework2/Android.mk
+++
@@ -1,6 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-BINDERDEMO_C_INCLUDES := frameworks/base/include/ \
-    system/core/include/
-
-include $(call all-makefiles-under,$(LOCAL_PATH))
